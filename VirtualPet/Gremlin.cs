e0e720a03525b360c1c0e9461cdc95bd762a4eba
andrewhoon <hoonandrew1709@gmail.com>
2017-01-20 18:05:12 -0500

    core of the project done

--- VirtualPet/Gremlin.cs
+++ VirtualPet/Gremlin.cs
@@ -8,6 +8,82 @@ namespace VirtualPet
 {
     class Gremlin
     {
+        private string name;
+        private int aggression = 50;
+        private int hydrationLevel = 50;
+        private int happiness = 50;
+        private int hungerLevel = 50;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Aggression
+        {
+            get { return this.aggression; }
+            set { this.aggression = value; }
+
+        }
+
+        public int HydrationLevel
+        {
+            get { return this.hydrationLevel; }
+            set { this.hydrationLevel = value; }
+        }
+
+        public int Happiness
+        {
+            get { return this.happiness; }
+            set { this.happiness = value; }
+        }
+
+        public int HungerLevel
+        {
+            get { return this.hungerLevel; }
+            set { this.hungerLevel = value; }
+        }
+
+        public void GetWater()
+        {
+         //put a random in here as to get different results
+            this.aggression += 20;
+            this.hydrationLevel -= 20;
+            this.happiness += 10;
+        }
+
+        public void GetFood()
+        {
+            this.aggression -= 20;
+            this.hydrationLevel += 20;
+            this.hungerLevel -= 35;
+            this.happiness += 15;
+        }
+
+        public void DoNothing()
+        {
+            this.aggression -= 10;
+            this.hydrationLevel += 10;
+            this.hungerLevel += 10;
+            this.happiness -= 5;
+        }
+
+        public void PlayTime()
+        {
+            this.aggression += 10;
+            this.hydrationLevel += 5;
+            this.hungerLevel += 5;
+            this.happiness += 25;
+        }
+
+        public void Monster()
+        {
+            if (this.aggression >= 100)
+            {
+                Console.WriteLine(this.name + " has gotten too aggressive and is now a monster! \nYou may need to feed it or leave it alone to get back to normal!");
+            }
+        }
 
     }
 }
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,7 +10,82 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            Gremlin gremlin1 = new Gremlin();
+            Console.WriteLine("Welcome to the Gremlim-Sitter game!");
+            Console.WriteLine("Please enter the name you wish to give to your Gremlin.");
+            gremlin1.Name = Console.ReadLine();
 
-        }
+            if (gremlin1.Name.ToUpper() == "GIZMO")
+            {
+                Console.WriteLine("Everyone always picks Gizmo.  Oh well, Gizmo it is.");
+            }
+            else
+            {
+                Console.WriteLine(gremlin1.Name + " is a great name!");
+            }
+
+
+            menu:
+            
+                Console.WriteLine("\n" + gremlin1.Name + " currently is at:");  //\n is to give a space to make console more readable
+                Console.WriteLine("Aggression level: " + gremlin1.Aggression + ".");
+                Console.WriteLine("Thirst level: " + gremlin1.HydrationLevel + ".");
+                Console.WriteLine("Happiness level: " + gremlin1.Happiness + ".");
+                Console.WriteLine("Hunger level: " + gremlin1.HungerLevel + ".\n");
+                Console.WriteLine("Enter \"Feed\" or \"F\" to give " + gremlin1.Name + " some food.");
+                Console.WriteLine("Enter \"Water\" or \"W\" to give " + gremlin1.Name + " some water.");
+                Console.WriteLine("Enter \"Play\" or \"P\" to play with " + gremlin1.Name + ".");
+                Console.WriteLine("Enter \"Quit\" or \"Q\" to quit.");
+                Console.WriteLine("Enter any other letter or letters to do nothing.\n \n \n \n \n \n");  //I did a lot of \n's to really increase readability
+                string response = Console.ReadLine();
+
+
+
+            switch (response.ToUpper())
+            {
+                case "W":
+                    gremlin1.GetWater();
+                    break;
+                case "WATER":
+                    gremlin1.GetWater();
+                    break;
+                case "F":
+                    gremlin1.GetFood();
+                    break;
+                case "FOOD":
+                    gremlin1.GetFood();
+                    break;
+                case "P":
+                    gremlin1.PlayTime();
+                    break;
+                case "PLAY":
+                    gremlin1.PlayTime();
+                    break;
+                case "Q":
+                    Environment.Exit(0);
+                    break;
+                case "QUIT":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    gremlin1.DoNothing();
+                    break;
+
+            }
+            gremlin1.Monster();
+
+            goto menu;
+           
+               
+
+
+
+
+
+
+            }
+
+        
+
     }
 }
