ef60898bc6c97041edc9f4e68ede96be3cd60136
andrewhoon <hoonandrew1709@gmail.com>
2017-01-21 02:50:19 -0500

    addedgamesavefunctionality

--- VirtualPet/Gremlin.cs
+++ VirtualPet/Gremlin.cs
@@ -55,9 +55,9 @@ namespace VirtualPet
 
         public void GetFood()
         {
-            this.aggression -= 20;
-            this.hydrationLevel += 20;
-            this.hungerLevel -= 35;
+            this.aggression -= 10;
+            this.hydrationLevel += 13;
+            this.hungerLevel -= 18;
             this.happiness += 15;
         }
 
@@ -79,9 +79,63 @@ namespace VirtualPet
 
         public void Monster()
         {
-            if (this.aggression >= 100)
+            if (this.aggression >= 80  && this.aggression < 100)
             {
                 Console.WriteLine(this.name + " has gotten too aggressive and is now a monster! \nYou may need to feed it or leave it alone to get back to normal!");
+            }
+            else if (this.aggression >= 100)
+            {
+                Console.WriteLine("You have killed " + this.name + " due to being over aggressive. " + this.name + "'s\nlittle heart couldn\'t take it.");
+                Environment.Exit(0);
+            }
+            else if (this.aggression <= 20 && this.aggression > 0)
+            {
+                Console.WriteLine("You may want to play with " + this.name + " before this gremlin dies of boredom!");
+            }
+            else if(this.aggression <= 0)
+            {
+                Console.WriteLine(this.name + " ,unfortunately, has died of boredom.");
+                Environment.Exit(0);
+            }
+
+        }
+
+        public void Starving()
+        {
+            if (this.hungerLevel >= 80 && this.hungerLevel < 100)
+            {
+                Console.WriteLine("Your gremlin is starving!  Please feed " + this.name + " immediately!");
+            }
+            else if (this.hungerLevel >=100)
+            {
+                Console.WriteLine("Since you never fed your gremlin, we had to take " + this.name + " into protective\ncustody and away from you. You\'ll never see " + this.name + " again!");
+                Environment.Exit(0);
+            }
+            else if (this.hungerLevel <= 20 && this.hungerLevel > 0)
+            {
+                Console.WriteLine("You may want to put your Gremlin on a diet, " + this.name + " has a poor BMI.");
+            }
+            else if (this.hungerLevel <= 0)
+            {
+                Console.WriteLine("We just sent " + this.name + " to fat camp and away from enablers such as you.");
+                Environment.Exit(0);
+            }
+        }
+
+        public void PureJoy()
+        {
+            if (this.happiness <= 20 && this.happiness > 0)
+            {
+                Console.WriteLine("Your gremlin is quite miserable.  Do you not spend quality time together?");
+            }
+            else if (this.happiness <= 0)
+            {
+                Console.WriteLine("Your gremlin left you for someone more fun.");
+                Environment.Exit(0);
+            }
+            else if (this.happiness >= 100)
+            {
+                Console.WriteLine("There is no limit to how happy a gremlin can be!!!");
             }
         }
 
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace VirtualPet
 {
@@ -12,13 +13,40 @@ namespace VirtualPet
         {
             Gremlin gremlin1 = new Gremlin();
             Console.WriteLine("Welcome to the Gremlim-Sitter game!");
-            Console.WriteLine("Please enter the name you wish to give to your Gremlin.");
-            gremlin1.Name = Console.ReadLine();
+            Console.WriteLine("Do you already have a saved game?");
+            string existingOrNew = (Console.ReadLine());
+            if (existingOrNew.ToUpper() == "YES")
+            {
+                Console.WriteLine("Hit \"Enter\" to continue.");
+                StreamReader retrieveSave = new StreamReader("..\\..\\GameSave.txt");
+                int numberOfLines = 6;
+                string[] lineArray = new string[numberOfLines];
+                for (int i = 1; i < numberOfLines; i++)
+                {
+                    lineArray[i] = retrieveSave.ReadLine();
+                }
+                gremlin1.Name = (lineArray[1]);
+                gremlin1.Aggression = int.Parse(lineArray[2]);
+                gremlin1.HydrationLevel = int.Parse(lineArray[3]);
+                gremlin1.Happiness = int.Parse(lineArray[4]);
+                gremlin1.HungerLevel = int.Parse(lineArray[5]);
+                Console.ReadLine();
+                retrieveSave.Close();
+                Console.WriteLine("Welcome back " + gremlin1.Name + "!");
+                goto menu;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the name you wish to give to your Gremlin.");
+                gremlin1.Name = Console.ReadLine();
+            }
 
+           
             if (gremlin1.Name.ToUpper() == "GIZMO")
             {
                 Console.WriteLine("Everyone always picks Gizmo.  Oh well, Gizmo it is.");
             }
+
             else
             {
                 Console.WriteLine(gremlin1.Name + " is a great name!");
@@ -62,9 +90,27 @@ namespace VirtualPet
                     gremlin1.PlayTime();
                     break;
                 case "Q":
-                    Environment.Exit(0);
+                    StreamWriter gameSave = new StreamWriter("..\\..\\GameSave.txt");
+                    using (gameSave)
+                    {
+                        gameSave.WriteLine(gremlin1.Name);
+                        gameSave.WriteLine(gremlin1.Aggression);
+                        gameSave.WriteLine(gremlin1.HydrationLevel);
+                        gameSave.WriteLine(gremlin1.Happiness);
+                        gameSave.WriteLine(gremlin1.HungerLevel);
+                    }
+                        Environment.Exit(0);
                     break;
                 case "QUIT":
+                    StreamWriter gameSave2 = new StreamWriter("..\\..\\GameSave.txt");
+                    using (gameSave2)
+                    {
+                        gameSave2.WriteLine(gremlin1.Name);
+                        gameSave2.WriteLine(gremlin1.Aggression);
+                        gameSave2.WriteLine(gremlin1.HydrationLevel);
+                        gameSave2.WriteLine(gremlin1.Happiness);
+                        gameSave2.WriteLine(gremlin1.HungerLevel);
+                    }
                     Environment.Exit(0);
                     break;
                 default:
@@ -73,6 +119,8 @@ namespace VirtualPet
 
             }
             gremlin1.Monster();
+            gremlin1.Starving();
+            gremlin1.PureJoy();
 
             goto menu;
            
