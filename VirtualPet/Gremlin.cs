2b48b92905720ac2cdda081e100eb2a4101fc08c
andrewhoon <hoonandrew1709@gmail.com>
2017-01-22 19:50:29 -0500

    finalizedVirtualPet

--- VirtualPet/Gremlin.cs
+++ VirtualPet/Gremlin.cs
@@ -8,13 +8,14 @@ namespace VirtualPet
 {
     class Gremlin
     {
-        private string name;
-        private int aggression = 50;
+        private string name;        //field to set initial defaults for name and levels of aggression, thirst, etc.
+        private int aggression = 50;    //These private ints will be altered for gremlin1 in the properites
         private int hydrationLevel = 50;
         private int happiness = 50;
         private int hungerLevel = 50;
+        private int ticks = 0;
 
-        public string Name
+        public string Name      //these properties help us adjust the variable scores for the game.
         {
             get { return this.name; }
             set { this.name = value; }
@@ -45,47 +46,202 @@ namespace VirtualPet
             set { this.hungerLevel = value; }
         }
 
+        public int Ticks
+        {
+            get { return this.ticks; }
+            set { this.ticks = value; }
+        }
+
         public void GetWater()
         {
-         //put a random in here as to get different results
-            this.aggression += 20;
-            this.hydrationLevel -= 20;
-            this.happiness += 10;
+            //put a random in here as to get different results. Some results have a better chance of being selected than others.
+            Random ran = new Random();
+            int anyNumber = ran.Next(0, 11);
+            if (anyNumber <= 3)
+            {
+                this.aggression += 25;  //the higher the aggression level, the more likely the gremlin turns into a monster.
+                this.hydrationLevel -= 20;  //hydrationLevel lowering means thirst is lowering. The gremlin isn't as thirsy after.
+                this.happiness += 10;   //with happiness level, the higher the score the happier the gremlin is.
+                this.hungerLevel += 5;  //hungerLevel lowering means the gremlin is less hungry.
+                Console.WriteLine("Your gremlin appreciates being hydrated, but be careful he doesn't get too aggressive!");
+            }
+            else if (anyNumber <= 6)
+            {
+                this.aggression += 18;
+                this.hydrationLevel -= 10;
+                this.happiness += 3;
+                this.hungerLevel += 3;
+            }
+            else if (anyNumber <= 9)
+                {
+                this.aggression += 13;
+                this.hydrationLevel -= 12;
+                this.happiness -= 5;
+                this.hungerLevel += 10;
+                }
+            else
+            {
+                this.aggression += 32;  //even though he didn't accept the water, he is being aggressive.
+                this.happiness -= 16;
+                this.hungerLevel += 12;
+                Console.WriteLine("Gremlins generally cannot have too much water. This gremlin got angry and refuses.");
+            }
         }
 
-        public void GetFood()
+        public void GetFood()   //this method will be called if the user selects feed from the menu
         {
-            this.aggression -= 10;
-            this.hydrationLevel += 13;
-            this.hungerLevel -= 18;
-            this.happiness += 15;
+            Random ran = new Random();
+            int anyNumber = ran.Next(0, 11);
+            if (anyNumber <= 3)
+            {
+                this.aggression -= 10;
+                this.hydrationLevel += 13;
+                this.hungerLevel -= 20;
+                this.happiness += 15;
+                Console.WriteLine("Your gremlin thinks that food really hit the spot!");
+            }
+            else if (anyNumber <= 9)
+            {
+                this.aggression -= 20;
+                this.hydrationLevel += 9;
+                this.hungerLevel -= 16;
+                this.happiness += 12;
+            }
+            else
+            {
+                this.aggression += 10;
+                this.happiness -= 20;
+                Console.WriteLine("Your gremlin refuses to eat.");
+            }
         }
 
-        public void DoNothing()
+        public void DoNothing()  //this method will be called when the user doesn't select another offered option in menu
         {
-            this.aggression -= 10;
-            this.hydrationLevel += 10;
-            this.hungerLevel += 10;
-            this.happiness -= 5;
+            Random ran = new Random();
+            int anyNumber = ran.Next(0, 11);
+            if (anyNumber <= 5)
+            {
+                this.aggression -= 10;
+                this.hydrationLevel += 10;
+                this.hungerLevel += 10;
+                this.happiness -= 5;
+            }
+            else
+            {
+                this.aggression -= 25;
+                this.hydrationLevel += 2;
+                this.hungerLevel += 2;
+                this.happiness -= 20;
+                Console.WriteLine("Your gremlin is a little more relaxed now.");
+            }
+
         }
 
-        public void PlayTime()
+        public void PlayTime()  //this method will be called from the menu if the user selects "play"
         {
-            this.aggression += 10;
-            this.hydrationLevel += 5;
-            this.hungerLevel += 5;
-            this.happiness += 25;
+            Random ran = new Random();
+            int anyNumber = ran.Next(0, 11);
+            if (anyNumber <= 3)
+            {
+                this.aggression += 10;
+                this.hydrationLevel += 5;
+                this.hungerLevel += 5;
+                this.happiness += 25;
+                Console.WriteLine("Your gremlin loves playtime!");
+            }
+            else if (anyNumber <= 6)
+            {
+                this.aggression += 6;
+                this.hydrationLevel += 10;
+                this.hungerLevel += 10;
+                this.happiness += 17;
+            }
+            else if (anyNumber <= 9)
+            {
+                this.aggression += 4;
+                this.hydrationLevel += 14;
+                this.hungerLevel += 15;
+                this.happiness += 12;
+            }
+            else
+            {
+                this.aggression += 20;
+                this.happiness -= 30;
+                Console.WriteLine("Your gremlin does not want to play right now.");
+            }
         }
 
-        public void Monster()
+            public void WaterDump() //this particular method is always bad. You should not dump water onto a gremlin.
         {
-            if (this.aggression >= 80  && this.aggression < 100)
+            Random ran = new Random();
+            int anyNumber = ran.Next(0, 1);
+            if (anyNumber == 0)
+            {
+                this.aggression = 99;
+                this.happiness = 20;
+                this.hungerLevel += 15;
+                Console.WriteLine("You really shouldn't do that. " + this.Name + " is now a monster!");
+            }
+            else
+            {
+                this.aggression = 99;
+                this.happiness = 10;
+                this.hungerLevel += 20;
+                Console.WriteLine("You have unleashed a monster into the world! Feeding it may calm it down.");
+            }
+
+        }
+
+
+        public void TicksTaken()        //this adds one every time a menu option is selected.  Every 5 plays will result in 
+        {                               //the gremlin perfoming some action on its own.
+            this.ticks ++;
+            if (this.ticks % 5 == 0)
+            {
+                Random ran = new Random();
+                int anyNumber = ran.Next(0, 2);
+
+                if (anyNumber == 0)
+                {
+                    this.aggression -= 15;
+                    this.hungerLevel += 5;
+                    this.hydrationLevel += 5;
+                    this.happiness += 5;
+                    Console.WriteLine(this.Name + " has taken a short nap but is now awake.");
+                }
+                else if (anyNumber == 1)
+                {
+                    this.aggression += 20;
+                    this.hungerLevel += 20;
+                    this.hydrationLevel -= 5;
+                    this.happiness -= 20;
+                    Console.WriteLine("Your gremlin has accidentally spilled water on itself!");
+                }
+                else
+                {
+                    this.aggression -= 5;
+                    this.hungerLevel -= 15;
+                    this.hydrationLevel += 3;
+                    this.happiness += 6;
+                    Console.WriteLine(this.Name + " has fed itself without you knowing.");
+                }
+            }
+        }
+
+        public void Monster()           //Gremlin turns into a monster at 80. Gremlin dies if aggression get to 100 or above and game ends.
+        {                               //Unfortunately, the gremlin can also die of boredom if the score reaches 0 or less.
+            if (this.aggression >= 80  && this.aggression < 100)    //this is determined by level of variable "aggression"
             {
                 Console.WriteLine(this.name + " has gotten too aggressive and is now a monster! \nYou may need to feed it or leave it alone to get back to normal!");
             }
             else if (this.aggression >= 100)
             {
                 Console.WriteLine("You have killed " + this.name + " due to being over aggressive. " + this.name + "'s\nlittle heart couldn\'t take it.");
+                Console.WriteLine("Aggression level: " + this.aggression + ". TOO AGGRESSIVE!"); //writing these since they won't go to the menu.
+                Console.WriteLine("Thirst level: " + this.hydrationLevel + ".");
+                Console.WriteLine("Happiness level: " + this.happiness + ".");
+                Console.WriteLine("Hunger level: " + this.hungerLevel + ".\n");
+                Console.WriteLine("Total ticks were " + this.ticks + ".");  //let people know how many times they played
                 Environment.Exit(0);
             }
             else if (this.aggression <= 20 && this.aggression > 0)
@@ -95,20 +251,30 @@ namespace VirtualPet
             else if(this.aggression <= 0)
             {
                 Console.WriteLine(this.name + " ,unfortunately, has died of boredom.");
+                Console.WriteLine("Aggression level: " + this.aggression + ". TOO BORED!");
+                Console.WriteLine("Thirst level: " + this.hydrationLevel + ".");
+                Console.WriteLine("Happiness level: " + this.happiness + ".");
+                Console.WriteLine("Hunger level: " + this.hungerLevel + ".\n");
+                Console.WriteLine("Total ticks were " + this.ticks + ".");
                 Environment.Exit(0);
             }
 
         }
 
-        public void Starving()
+        public void Starving()  //this is determined by the level of variable "hungerLevel"
         {
-            if (this.hungerLevel >= 80 && this.hungerLevel < 100)
-            {
+            if (this.hungerLevel >= 80 && this.hungerLevel < 100)   //Gremlin can starve with a score of 100 or more
+            {                                                       //Conversely, the gremlin can get too fat and be removed, ending the game.
                 Console.WriteLine("Your gremlin is starving!  Please feed " + this.name + " immediately!");
             }
             else if (this.hungerLevel >=100)
             {
                 Console.WriteLine("Since you never fed your gremlin, we had to take " + this.name + " into protective\ncustody and away from you. You\'ll never see " + this.name + " again!");
+                Console.WriteLine("Aggression level: " + this.aggression + ".");
+                Console.WriteLine("Thirst level: " + this.hydrationLevel + ".");
+                Console.WriteLine("Happiness level: " + this.happiness + ".");
+                Console.WriteLine("Hunger level: " + this.hungerLevel + ". TOO HUNGRY! \n");
+                Console.WriteLine("Total ticks were " + this.ticks + ".");
                 Environment.Exit(0);
             }
             else if (this.hungerLevel <= 20 && this.hungerLevel > 0)
@@ -118,19 +284,29 @@ namespace VirtualPet
             else if (this.hungerLevel <= 0)
             {
                 Console.WriteLine("We just sent " + this.name + " to fat camp and away from enablers such as you.");
+                Console.WriteLine("Aggression level: " + this.aggression + ".");
+                Console.WriteLine("Thirst level: " + this.hydrationLevel + ".");
+                Console.WriteLine("Happiness level: " + this.happiness + ".");
+                Console.WriteLine("Hunger level: " + this.hungerLevel + ". FED TOO MUCH! \n");
+                Console.WriteLine("Total ticks were " + this.ticks + ".");
                 Environment.Exit(0);
             }
         }
 
-        public void PureJoy()
+        public void PureJoy()   //this is determined by level of the variable "happiness"
         {
-            if (this.happiness <= 20 && this.happiness > 0)
-            {
+            if (this.happiness <= 20 && this.happiness > 0) //the gremlin happiness level can get too low and the gremlin can leave you.
+            {                                               //however, the gremlin cannot get too happy.  The happier, the better.
                 Console.WriteLine("Your gremlin is quite miserable.  Do you not spend quality time together?");
             }
             else if (this.happiness <= 0)
             {
                 Console.WriteLine("Your gremlin left you for someone more fun.");
+                Console.WriteLine("Aggression level: " + this.aggression + ".");
+                Console.WriteLine("Thirst level: " + this.hydrationLevel + ".");
+                Console.WriteLine("Happiness level: " + this.happiness + ". HAPPINESS TOO LOW!");
+                Console.WriteLine("Hunger level: " + this.hungerLevel + ".\n");
+                Console.WriteLine("Total ticks were " + this.ticks + ".");
                 Environment.Exit(0);
             }
             else if (this.happiness >= 100)
@@ -139,5 +315,27 @@ namespace VirtualPet
             }
         }
 
+        public void Dehydration()       //this is determined by level of variable "hydrationLevel"
+        {                               //you can dehydrate your gremlin and end the game with a level of 0.
+            if (this.hydrationLevel <= 20)   //however, since water effects aggression so much, you can't
+            {                                                           //end the game in this method. Most likely it will end
+                Console.WriteLine("Gremlins do not like too much water. It can make them aggressive."); //in the method "Monster".
+            }
+            else if (this.hydrationLevel >= 80 && this.hydrationLevel < 100)
+            {
+                Console.WriteLine("You don\'t want your gremlin to dehydrate! You better get it some water");
+            }
+            else if (this.hydrationLevel >= 100)
+            {
+                Console.WriteLine(this.Name + " has dehydrated and your game has ended.");
+                Console.WriteLine("Aggression level: " + this.aggression + ".");
+                Console.WriteLine("Thirst level: " + this.hydrationLevel + ". YOUR GREMLIN WAS THIRSTY!");
+                Console.WriteLine("Happiness level: " + this.happiness + ".");
+                Console.WriteLine("Hunger level: " + this.hungerLevel + ".\n");
+                Console.WriteLine("Total ticks were " + this.ticks + ".");
+                Environment.Exit(0);
+            }
+
+        }
     }
 }
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -12,28 +12,29 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             Gremlin gremlin1 = new Gremlin();
-            Console.WriteLine("Welcome to the Gremlim-Sitter game!");
-            Console.WriteLine("Do you already have a saved game?");
-            string existingOrNew = (Console.ReadLine());
-            if (existingOrNew.ToUpper() == "YES")
+            Console.WriteLine("Welcome to the Gremlin-Sitter game!");
+            Console.WriteLine("Do you already have a saved game?"); //you will need to have played and quit on your own for this to load
+            string existingOrNew = (Console.ReadLine());            //console will crash if you select "yes" and do not have a save.
+
+            if (existingOrNew.ToUpper() == "YES" || existingOrNew.ToUpper() == "Y")
             {
-                Console.WriteLine("Hit \"Enter\" to continue.");
-                StreamReader retrieveSave = new StreamReader("..\\..\\GameSave.txt");
+                Console.WriteLine("Hit \"Enter\" to continue.");    //for some reason, to get my gamesave to work it needs
+                StreamReader retrieveSave = new StreamReader("..\\..\\GameSave.txt");   //another input of "enter" to continue.
                 int numberOfLines = 6;
-                string[] lineArray = new string[numberOfLines];
+                string[] lineArray = new string[numberOfLines]; //taking GameSave.txt and turning lines into an array to read them
                 for (int i = 1; i < numberOfLines; i++)
                 {
                     lineArray[i] = retrieveSave.ReadLine();
                 }
-                gremlin1.Name = (lineArray[1]);
-                gremlin1.Aggression = int.Parse(lineArray[2]);
+                gremlin1.Name = (lineArray[1]);     //these are using the properties to change the variable amounts and string name
+                gremlin1.Aggression = int.Parse(lineArray[2]);  //all lines read as a string, so Parse will get them into an int
                 gremlin1.HydrationLevel = int.Parse(lineArray[3]);
                 gremlin1.Happiness = int.Parse(lineArray[4]);
                 gremlin1.HungerLevel = int.Parse(lineArray[5]);
                 Console.ReadLine();
                 retrieveSave.Close();
                 Console.WriteLine("Welcome back " + gremlin1.Name + "!");
-                goto menu;
+                goto menu;  //this sends it to the main menu
             }
             else
             {
@@ -41,8 +42,9 @@ namespace VirtualPet
                 gremlin1.Name = Console.ReadLine();
             }
 
+
            
-            if (gremlin1.Name.ToUpper() == "GIZMO")
+            if (gremlin1.Name.ToUpper() == "GIZMO") //Gizmo is the name in the movies. If this is selected, they get a nice little message
             {
                 Console.WriteLine("Everyone always picks Gizmo.  Oh well, Gizmo it is.");
             }
@@ -53,8 +55,9 @@ namespace VirtualPet
             }
 
 
-            menu:
-            
+
+            menu:   //using this instead of a loop because the program seems small enough to find and
+                                                                    //because i got my "gamesave" to work with it.
                 Console.WriteLine("\n" + gremlin1.Name + " currently is at:");  //\n is to give a space to make console more readable
                 Console.WriteLine("Aggression level: " + gremlin1.Aggression + ".");
                 Console.WriteLine("Thirst level: " + gremlin1.HydrationLevel + ".");
@@ -63,13 +66,14 @@ namespace VirtualPet
                 Console.WriteLine("Enter \"Feed\" or \"F\" to give " + gremlin1.Name + " some food.");
                 Console.WriteLine("Enter \"Water\" or \"W\" to give " + gremlin1.Name + " some water.");
                 Console.WriteLine("Enter \"Play\" or \"P\" to play with " + gremlin1.Name + ".");
-                Console.WriteLine("Enter \"Quit\" or \"Q\" to quit.");
+                Console.WriteLine("Enter \"Dump\" or \"D\" to dump water onto " + gremlin1.Name + ".");
+                Console.WriteLine("Enter \"Quit\" or \"Q\" to quit and save game.");
                 Console.WriteLine("Enter any other letter or letters to do nothing.\n \n \n \n \n \n");  //I did a lot of \n's to really increase readability
                 string response = Console.ReadLine();
 
 
 
-            switch (response.ToUpper())
+            switch (response.ToUpper()) //this takes their entry and the switch case will select which method to call on.
             {
                 case "W":
                     gremlin1.GetWater();
@@ -89,48 +93,53 @@ namespace VirtualPet
                 case "PLAY":
                     gremlin1.PlayTime();
                     break;
+                case "D":
+                    gremlin1.WaterDump();
+                    break;
+                case "DUMP":
+                    gremlin1.WaterDump();
+                    break;
                 case "Q":
-                    StreamWriter gameSave = new StreamWriter("..\\..\\GameSave.txt");
-                    using (gameSave)
-                    {
+                    StreamWriter gameSave = new StreamWriter("..\\..\\GameSave.txt");  //this will write or overwrite the txt 
+                    using (gameSave)                                                    //that will be used to read when console is
+                    {                                                                   //reopened and saved game is selected.
                         gameSave.WriteLine(gremlin1.Name);
                         gameSave.WriteLine(gremlin1.Aggression);
                         gameSave.WriteLine(gremlin1.HydrationLevel);
                         gameSave.WriteLine(gremlin1.Happiness);
                         gameSave.WriteLine(gremlin1.HungerLevel);
                     }
+                    Console.WriteLine("Total ticks were " + gremlin1.Ticks + ".");  //let people know how many times they entered a selection
                         Environment.Exit(0);
                     break;
                 case "QUIT":
                     StreamWriter gameSave2 = new StreamWriter("..\\..\\GameSave.txt");
                     using (gameSave2)
                     {
-                        gameSave2.WriteLine(gremlin1.Name);
-                        gameSave2.WriteLine(gremlin1.Aggression);
+                        gameSave2.WriteLine(gremlin1.Name);     //this does the same as case Q. Had to make a second StreamWriter
+                        gameSave2.WriteLine(gremlin1.Aggression);   //but result is the same.
                         gameSave2.WriteLine(gremlin1.HydrationLevel);
                         gameSave2.WriteLine(gremlin1.Happiness);
                         gameSave2.WriteLine(gremlin1.HungerLevel);
                     }
+                    Console.WriteLine("Total ticks were " + gremlin1.Ticks + ".");
                     Environment.Exit(0);
                     break;
                 default:
-                    gremlin1.DoNothing();
+                    gremlin1.DoNothing();   //if none of the menu options are selected, it will default to "do nothing"
                     break;
 
             }
+
+            //these are the methods that are always called on after the menu selection and the changing of values in
+            //gremlin1's parameters.  These determine if the paramaters are either too high or too low.
+            gremlin1.TicksTaken();  //this method has to be run first in case it effects the numbers that are used in other methods
             gremlin1.Monster();
             gremlin1.Starving();
             gremlin1.PureJoy();
+            gremlin1.Dehydration();
 
-            goto menu;
-           
-               
-
-
-
-
-
-
+            goto menu;  //this brings us back to the main menu
             }
 
         
